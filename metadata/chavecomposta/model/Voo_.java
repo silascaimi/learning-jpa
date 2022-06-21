7ee5b2c3c51fc10b45f22653b3b3c8115e27d53d
Silas Caimi <silascaimi@live.com>
2022-06-21 20:18:50 -0300

    desconsiderar: metadata chave composta

--- metadata/chavecomposta/model/Voo_.java
+++ metadata/chavecomposta/model/Voo_.java
@@ -4,11 +4,11 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2022-06-09T21:27:02.452-0300")
+@Generated(value="Dali", date="2022-06-21T19:55:48.859-0300")
 @StaticMetamodel(Voo.class)
 public class Voo_ {
 	public static volatile SingularAttribute<Voo, Integer> numero;
 	public static volatile SingularAttribute<Voo, String> empresa;
-	public static volatile SingularAttribute<Voo, String> destino;
 	public static volatile SingularAttribute<Voo, String> origem;
+	public static volatile SingularAttribute<Voo, String> destino;
 }
