8cf180dbfb876f0aea3e4f012774ec165ca17c5f
Silas Caimi <silascaimi@live.com>
2022-07-20 10:20:00 -0300

    Persistence Context Extended

---
+++ src/main/java/controleTransacional/extendedpersistence/bean/FormBean.java
@@ -0,0 +1,62 @@
+package controleTransacional.extendedpersistence.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import controleTransacional.extendedpersistence.ejb.ItensBean;
+import controleTransacional.extendedpersistence.model.Item;
+
+@Named("extended")
+@SessionScoped
+public class FormBean {
+
+	@EJB
+	private ItensBean itensBean;
+	
+	@Inject
+	private ExternalContext ec;
+	
+	private List<Item> itens = new ArrayList<>();
+	
+	private Item item = new Item();
+	
+	public String adicionar() {
+		itens.add(item);
+		itensBean.adcidionarItem(item);
+		item = new Item();
+		return "extendedpersistence?faces-redirect=true";
+	}
+	
+	public String remover(Item item) {
+		itens.remove(item);
+		itensBean.removerItem(item.getId());
+		item = new Item();
+		return "extendedpersistence?faces-redirect=true";
+	}
+	
+	public String finalizar() {
+		itensBean.finalizar();
+		ec.invalidateSession(); // Mata a session após a conclusão
+		return "extendedpersistence?faces-redirect=true";
+	}
+	
+	public List<Item> getItens() {
+		return itens;
+	}
+	
+	public Item getItem() {
+		return item;
+	}
+	
+	public void setItem(Item item) {
+		this.item = item;
+	}
+	
+	
+}
---
+++ src/main/java/controleTransacional/extendedpersistence/ejb/ItensBean.java
@@ -0,0 +1,42 @@
+package controleTransacional.extendedpersistence.ejb;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Remove;
+import javax.ejb.Stateful;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
+
+import controleTransacional.extendedpersistence.model.Item;
+
+/**
+ * Session Bean implementation class ItensBean
+ */
+@Stateful
+@LocalBean
+@TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
+public class ItensBean {
+
+	@PersistenceContext(type = PersistenceContextType.EXTENDED)
+	private EntityManager em;
+	
+	public void adcidionarItem(Item item) {
+		em.persist(item);
+	}
+	
+	public void removerItem(int itemId) {
+		Item item = em.find(Item.class, itemId);
+		em.remove(item);
+	}
+	
+	// Excluio o EJB para efetivar as mudançar no banco
+	// Tudo que foi armazenado pela jpa é armazenado
+	@Remove
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	public void finalizar() {
+		System.out.println("Commit realizado");
+	}
+
+}
